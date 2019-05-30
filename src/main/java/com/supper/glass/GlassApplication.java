78107e6463cb8afbf9e736a105f83389fb2464e9
Supper <Supper@zhangdaxideMacBook-Pro.local>
2019-05-30 10:45:06 +0800

    Initial commit

---
+++ src/main/java/com/supper/glass/GlassApplication.java
@@ -0,0 +1,13 @@
+package com.supper.glass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlassApplication.class, args);
+    }
+
+}
